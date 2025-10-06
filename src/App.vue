07a109623e6d888bf510e0c3a00e86fb013b6c3e
Tomoaki Higa <tomoaki.higa@create-sekai.com>
2025-10-06 11:31:26 +0900

    fix: example code

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,48 @@
 <template>
   <div id="app">
-    <div style="height: 500px; width: 500px; border: 1px solid red; position: relative;">
-      <vue-draggable-resizable :w="400" :h="400" :parent="true" :min-width="200" :min-height="200">
+    <div
+      :style="`height: ${height}px; width: ${width}px; border: 1px solid red; position: relative;`"
+    >
+      <vue-draggable-resizable
+        :w="200"
+        :h="200"
+        :parent="true"
+        :min-width="200"
+        :min-height="200"
+      >
         <p>vue-draggable-resizable</p>
       </vue-draggable-resizable>
     </div>
+    <button @click="width += 50">Increase width</button>
+    <button @click="width -= 50">Decrease width</button>
+
+    <button @click="height += 50">Increase height</button>
+    <button @click="height -= 50">Decrease height</button>
+
+    <button @click="()=>{ width = 500; height = 500;}">Reset</button>
   </div>
 </template>
 
 <script>
-import VueDraggableResizable from './components/vue-draggable-resizable.vue'
-import './components/vue-draggable-resizable.css'
+import VueDraggableResizable from "./components/vue-draggable-resizable.vue";
+import "./components/vue-draggable-resizable.css";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    VueDraggableResizable
-  }
-}
+    VueDraggableResizable,
+  },
+  data() {
+    return {
+      width: 500,
+      height: 500,
+    };
+  },
+};
 </script>
 
 <style>
-  .vdr {
-    border: 1px dashed black;
-  }
+.vdr {
+  border: 1px dashed black;
+}
 </style>
